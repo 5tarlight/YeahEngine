e8944cd050c024bd383f4b717dac0fde20ed0557
YEAHx4 <developerstarlight@gmail.com>
2022-02-23 14:05:00 +0900

    init javafx

--- app/src/main/java/yengine/game/fx/Yengine.java
+++ app/src/main/java/yengine/game/fx/Yengine.java
@@ -1,24 +1,19 @@
-package yengine;
+package yengine.game.fx;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import yengine.game.fx.Yengine;
 
-public class App extends Application {
+public class Yengine extends Application {
   @Override
-  public void start(Stage stage) {
+  public void start(Stage stage) throws Exception {
     String javaVersion = System.getProperty("java.version");
     String javafxVersion = System.getProperty("javafx.version");
     Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
     Scene scene = new Scene(new StackPane(l), 640, 480);
     stage.setScene(scene);
     stage.show();
-  }
-
-  public static void main(String[] args) {
-    launch();
   }
 }
